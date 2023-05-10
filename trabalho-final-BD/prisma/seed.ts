de2e5542a1630b8819ac6116948685b8c58fc4df
Kennedy <williankennedy368@gmail.com>
2023-05-10 20:54:05 -0300

    consultas feitas

--- trabalho-final-BD/prisma/seed.ts
+++ trabalho-final-BD/prisma/seed.ts
@@ -1,8 +1,8 @@
-import { faker} from '@faker-js/faker';
+//import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-const fakers = faker
+//const fakers = faker
 
 async function main() {
     //criando alguns times
@@ -16,7 +16,7 @@ async function main() {
     // ]
 
     //função para cadastrar os developers
-    // for (let i = 0; i < developers.length; i++) {
+    //for (let i = 0; i < developers.length; i++) {
     //     await prisma.developer.create({
     //         data:{
     //             name: developers[i]
@@ -26,24 +26,96 @@ async function main() {
 
     //popular sprint
 
-    for (let i = 0; i < 2; i++) {
-        const date = new Date(fakers.date.between('2023-05-01','2023-05-10'))
-        console.log(date)
-        const date_new = new Date(date)
-        let intervalo = Math.floor(Math.random() * 14) + 14
-        console.log(intervalo)
-        const conclusion = new Date(date.setDate(date.getDate() + intervalo))
-        console.log(conclusion)
-        await prisma.sprint.create({
-            data:{
-                description: fakers.lorem.sentence(),
-                start: date_new,
-                prev_conclusion: conclusion
-            }
-        })
-    }
+    // for (let i = 0; i < 2; i++) {
+    //     const date = new Date(fakers.date.between('2023-05-01','2023-05-10'))
+    //     const date_new = new Date(date)
+    //     let intervalo = Math.floor(Math.random() * 14) + 14
+    //     const conclusion = new Date(date.setDate(date.getDate() + intervalo))
+    //     await prisma.sprint.create({
+    //         data:{
+    //             description: fakers.lorem.sentence(),
+    //             start: date_new,
+    //             prev_conclusion: conclusion
+    //         }
+    //     })
+    // }
 
     //colocar issue
+    // let issues_Status: string[] = [
+    //     'A fazer',
+    //     'Fazendo',
+    //     'Feito'
+    // ];
+
+    // let prioritys: string[] =[
+    //     'alta', 
+    //     'média',
+    //     'baixa'
+    // ]
+    // let n = 6
+    // while(n>0){
+    //     const date = new Date(fakers.date.between('2023-05-10','2023-05-18')) 
+    //     const date_new = new Date(date)
+    //     let intervalo = Math.floor(Math.random() * 14) + 14
+    //     const conclusion = new Date(date.setDate(date.getDate() + intervalo))
+
+
+    //     const id_sprints = await prisma.sprint.findMany()
+    //     await prisma.issue.create({
+    //         data:{
+    //             name_issue: fakers.random.words(4),
+    //             priority: prioritys[Math.floor(Math.random()*2.99) ],
+    //             start: date_new,
+    //             conclusion: conclusion ,
+    //             status: issues_Status[Math.floor(Math.random() * 2.99)],
+    //             id_sprint: +id_sprints[Math.floor(Math.random() * 1.99)].id
+    //         }
+    //     })
+    //     n--
+    // }
+    // deixando nulos as datas de conclusão das issues não concluidas
+    // let issues_modificar: number[]
+    // const issues_em_andamento = await prisma.issue.findMany({
+    //     where:{
+    //         status: "A fazer" || "Fazendo"
+    //     }
+    // })
+
+    // for(let i = 0; issues_em_andamento.length; i++){
+    //     issues_modificar.push(issues_em_andamento[i].number);
+    // }
+    
+    // for(let i = 0; issues_modificar.length; i++ ){
+    //     const update = await prisma.issue.update({
+    //         where: {
+    //             number: issues_modificar[i]
+    //         },
+    //         data: {
+    //             conclusion: null
+    //         },
+    //     })
+    //     console.log(update)
+    // }
+    //relation WorkAt
+
+    // const recebeIdIssues = await prisma.issue.findMany({
+    //     where: {
+    //         status: "Feito" || "Fazendo"
+    //     }
+    // })
+    
+    // const recebeDeveloper  = await prisma.developer.findMany()
+    
+    // for(let i = 0; i< recebeIdIssues.length ;i++){
+    //     await prisma.workAt.create({
+    //         data: {
+    //             cod_dev : +recebeDeveloper[Math.floor(Math.random() * recebeDeveloper.length)].cod,
+    //             number_issue : +recebeIdIssues[i].number
+    //         }
+
+    //     })
+    // }
+
     
 }
 
--- trabalho-final-BD/src/query.ts
+++ trabalho-final-BD/src/query.ts
@@ -1,16 +1,147 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function main(){
-	//adicionaremos nossas consultas aqui
+async function maisDe70(sprint: Number) {
+	const result_7 = await prisma.issue.findMany({
+		where: {
+			id_sprint: +sprint
+		}
+	})
+
+	const result_8 = await prisma.issue.findMany({
+		where: {
+			status: "Feito",
+			id_sprint: +sprint
+		}
+	})
+
+	if ((result_8.length / result_7.length) > 0.7) {
+		return true
+	} else {
+		return false
+	}
+
 }
 
+async function concluida(sprint: Number) {
+	const result_7 = await prisma.issue.findMany({
+		where: {
+			id_sprint: +sprint
+		}
+	})
+
+	const result_8 = await prisma.issue.findMany({
+		where: {
+			status: "Feito",
+			id_sprint: +sprint
+		}
+	})
+
+	if ((result_8.length / result_7.length) == 1) {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+async function main() {
+	//adicionaremos nossas consultas aqui
+	// Quantas sprints foram planejadas
+	console.log('Consulta 1:')
+	const result_1 = await prisma.sprint.findMany()
+	console.log(result_1.length)
+	console.log("-------------")
+
+	//Quantas issues existem relacionadas a uma determinada sprint
+	console.log('Consulta 2:')
+	const result_2 = await prisma.issue.findMany({
+		where: {
+			id_sprint: 9
+		}
+	})
+	console.log(result_2)
+	console.log("-------------")
+
+	//Para uma sprint quais as issues que estão para ser feitas e que encontram-se feitas
+	console.log('Consulta 3:')
+	const result_3 = await prisma.issue.findMany({
+		where: {
+			id_sprint: 9,
+			status: "A fazer"
+		}
+	})
+
+	const result = await prisma.issue.findMany({
+		where: {
+			id_sprint: 9,
+			status: "Feito"
+		}
+	})
+	console.log(result_3)
+	console.log(result)
+	console.log("-------------")
+
+	//Se a sprint está com mais de 70% das issues finalizadas
+	console.log('Consulta 4:')
+	let n = 10;
+	if (await maisDe70(n)) {
+		console.log(`O Sprint ${n} tem mais de 70% da Issues concluídas.`)
+	} else {
+		console.log(`O Sprint ${n} não possui mais de 70% das Issues concluídas.`)
+	}
+
+	console.log("-------------")
+
+
+	// Qual desenvolvedor mais fechou (concluiu) issues 
+	console.log('Consulta 5:')
+	
+	const developer = await prisma.workAt.groupBy({
+		by:['cod_dev'],
+		_count: {
+			number_issue: true
+		  },
+	})
+	let maior = 0;
+	let aux = developer[maior]._count;
+	for(let i = 0; i < developer.length; i++){
+		if(aux < developer[i]._count){
+			maior = i;
+
+		}
+	}
+	const maisTrabalhador = await prisma.developer.findFirst({
+		where:{
+			cod: developer[maior].cod_dev
+		}
+	})
+	console.log('O desenvolvedor que mais concluiu issues foi ' + JSON.stringify(maisTrabalhador))
+
+	// Para uma determina sprint concluída qual a sua data de conclusão
+	console.log('Consulta 6:')
+	n = 10;
+	if (await concluida(n)) {
+		const conclusion = await prisma.issue.findFirst({
+			orderBy:{
+				conclusion: 'asc'
+			}
+		})
+		console.log(`A issue ${n} foi concluiuda na data ${conclusion?.conclusion}`)
+	} else {
+		console.log(`A issue ${n} não foi concluiuda.`)
+	}
+
+	console.log("-------------")
+}
+
+
 main()
-	.then(async()=>{
+	.then(async () => {
 		await prisma.$disconnect()
 	})
-	.catch(async(e)=>{
+	.catch(async (e) => {
 		console.error(e)
 		await prisma.$disconnect()
 	})
